16e0d7909bf3e608e29c3ec0237e2b06b4e5e3fc
unknown <hafidamroucheg@gmail.com>
2024-09-03 12:34:51 +0100

    'commit'

--- hanotify_files/hostsconf/views.py
+++ hanotify_files/hostsconf/views.py
@@ -5,6 +5,7 @@ from django.conf import settings
 # delete user and delete category
 def render_store(request, path=None):
     domain = request.get_host()
+    print(domain, request.build_full_url())
     domain = domain.replace(':8080', '') # remove this line
     with open(settings.BASE_DIR / f'json/users/stores/{domain}.json', 'r') as json_file:
         store = json.load(json_file)
