4a275a84fa2a8844b219f93e913250b7df857f1b
unknown <hafidamroucheg@gmail.com>
2024-10-07 13:31:22 +0100

    'commit'

--- hanotify_files/hostsconf/urls.py
+++ hanotify_files/hostsconf/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.render_store),
+    path('categories/<str>', views.render_category),
     path('products/<slug>/<product_id>', views.render_product),
     path('products/<slug>/<product_id>/landing-page', views.render_product),
     path('products/<slug>/<product_id>/gallery', views.render_product),
